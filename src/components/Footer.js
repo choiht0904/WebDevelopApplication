84f5d9ca3859ae9ef817ca5f8729d23968782896
choiht0904 <choiht0904@naver.com>
2023-12-09 17:51:39 +0900

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ import Box from "@mui/material/Box";
 
 export default function Footer(props = { mt: 8, mb: 4 }) {
     return (
-        <Box sx={{position: 'fixed', bottom: 0, width: "100%", borderTop: "1px solid gray", minHeight: "100px", padding: "10px"}} component={"footer"}>
+        <Box sx={{position: 'sticky', bottom: 0, width: "100%", marginTop: "100px", borderTop: "1px solid gray", minHeight: "100px", maxHeight: "100px", padding: "10px", backgroundColor: "white", zIndex: 100}} component={"footer"}>
             <Typography variant="body2" color="text.secondary" align="center" {...props}>
                 {'Copyright © '}
-                <Link color="inherit" href="https://mui.com/">
+                <Link color="inherit" href="https://web.kangnam.ac.kr/index.do">
                     개가좋다
                 </Link>{' '}
                 {new Date().getFullYear()}
