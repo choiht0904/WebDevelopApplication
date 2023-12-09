129d493b40898b02833d5863e0d5baafdd9c0416
JYSoul <118038141+JYSoul@users.noreply.github.com>
2023-12-09 18:16:08 +0900

    Update ProductAdd.js

--- src/components/productadd/ProductAdd.js
+++ src/components/productadd/ProductAdd.js
@@ -6,6 +6,7 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -13,8 +14,19 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
 
 
+function Copyright(props) {
+  return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+          {'Copyright © '}
+          <Link color="inherit" href="https://mui.com/">
+              개가좋다
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+      </Typography>
+  );
+}
 const defaultTheme = createTheme();
-
 export default function SubmitOn() {
     const [imageSrc, setImageSrc] = React.useState('');
     const FileSelected = () => {
@@ -38,8 +50,14 @@ export default function SubmitOn() {
       variant="contained"
       component="label">
 
-        <input type="file" onChange={handleFileChange} />
-        {imageSrc && <img src={imageSrc} alt="Uploaded"/>}
+        <Input
+        type="file"
+        inputProps={{
+          onChange: handleFileChange,
+        }}
+        style={{ display: 'none' }}
+      />이미지 업로드
+      {imageSrc && <img src={imageSrc} alt="Uploaded" />}
       </Button>
     );
   };
@@ -56,86 +74,79 @@ export default function SubmitOn() {
           content: data.get('content'),
         };
   
-      axios.post(`http://${backend}/products`, productsData)
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    };
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}>
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        상품 등록
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={1}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    autoComplete="given-title"
-                                    name="title"
-                                    required
-                                    fullWidth
-                                    id="title"
-                                    label="Title"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="images"
-                                    label="Images Address"
-                                    name="images"
-                                    autoComplete="images"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="prices"
-                                    label="Prices"
-                                    type="prices"
-                                    id="prices"
-                                    autoComplete="new-prices"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="content"
-                                    label="Content"
-                                    type="content"
-                                    id="content"
-                                    autoComplete="new-content"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}>
-                            등록하기
-                        </Button>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+        // 서버로 데이터 전송
+        axios.post(`http://${backend}/products`, productsData)
+          .then(res => {
+            console.log(res.data);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      };
+  return (
+      <ThemeProvider theme={defaultTheme}>
+          <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box sx={{
+                      marginTop: 8,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                  }}>
+                  <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                      <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography component="h1" variant="h5">
+                      상품 등록
+                  </Typography>
+                  <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                      <Grid container spacing={1}>
+                          <Grid item xs={12}>
+                              <TextField
+                                  autoComplete="given-title"
+                                  name="title"
+                                  required
+                                  fullWidth
+                                  id="title"
+                                  label="Title"
+                                  autoFocus
+                              />
+                          </Grid>
+                          <FileSelected/>
+                          <Grid item xs={12}>
+                              <TextField
+                                  required
+                                  fullWidth
+                                  name="prices"
+                                  label="Price"
+                                  type="prices"
+                                  id="prices"
+                                  autoComplete="new-prices"
+                              />
+                          </Grid>
+                          <Grid item xs={12}>
+                              <TextField
+                                  required
+                                  fullWidth
+                                  name="content"
+                                  label="Content"
+                                  type="content"
+                                  id="content"
+                                  autoComplete="new-content"
+                              />
+                          </Grid>
+                      </Grid>
+                      <Button
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          sx={{ mt: 3, mb: 2 }}>
+                          등록하기
+                      </Button>
+                  </Box>
+              </Box>
+              <Copyright sx={{ mt: 5 }} />
+          </Container>
+      </ThemeProvider>
+  );
 }
